8934bfaa6c32de01f46f6b0017a53919fae5470c
yanyanshan <yanyanshan@bigo.sg>
2021-07-13 15:53:40 +0800

    feat：handleActions的多种写法

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -10,7 +10,7 @@ const actions = createActions({
     SET_ACCOUNT: data => data,
     SET_ALL: num => num,
     // 接口
-    GET_ACCOUNT:get=> get, // 获取账目
+    GET_ACCOUNT: get=> get, // 获取账目
     ADD_ACCOUNT: add => add, // 新增账目
     UPDATE_ACCOUNT: update => update, // 修改账目
     DELETE_ACCOUNT: del => del // 删除账目
@@ -18,5 +18,5 @@ const actions = createActions({
 console.log("action1:",action1());
 console.log("action2:",action2(100));
 console.log("actions:",actions);
-console.log("SET_LOADING",actions.setLoading); 
+console.log("SET_LOADING",actions.setLoading()); 
 export default actions;
--- src/store/reducer/account.js
+++ src/store/reducer/account.js
@@ -12,33 +12,51 @@ const defaultState = Immutable({
 // createAction: https://github.com/redux-utilities/redux-actions/blob/master/docs/api/createAction.md
 // combineActions: https://github.com/redux-utilities/redux-actions/blob/master/docs/api/combineActions.md
 // handleActions的使用方法：https://github.com/redux-utilities/redux-actions/blob/master/docs/api/handleAction.md
-const reducer = handleActions(
-    new Map([
-        [
-            actions.setLoading,
-            (state, action) =>
-            {
-                console.log("reducer里边的action",action);
-                return state.set("loading", action.payload)
-            }
+// 写法一：
+// const reducer = handleActions(
+//     new Map([
+//         [
+//             actions.setLoading,
+//             (state, action) =>
+//             {
+//                 console.log("reducer里边的action",action);
+//                 return state.set("loading", action.payload)
+//             }
            
-        ],
-        [
-            actions.setAccount,
-            (state, {
-                payload
-            }) => 
-            state.set("accounts", payload)
-        ],
-        [
-            actions.setAll,
-            (state, {
-                payload
-            }) =>
-            state.set("all", payload)
-        ]
-    ]),
-    defaultState
-);
-
+//         ],
+//         [
+//             actions.setAccount,
+//             (state, {
+//                 payload
+//             }) => 
+//             state.set("accounts", payload)
+//         ],
+//         [
+//             actions.setAll,
+//             (state, {
+//                 payload
+//             }) =>
+//             state.set("all", payload)
+//         ]
+//     ]),
+//     defaultState
+// );
+// 写法二：
+// const reducer = handleActions({
+//     'SET_LOADING': (state, action) => {
+//         console.log("reducer里边的action",action);
+//         return state.set("loading", action.payload)
+//     },
+//     'SET_ACCOUNT': (state, {payload}) =>  state.set("accounts", payload),
+//     'SET_ALL': (state, {payload}) =>  state.set("all", payload)
+//   }, defaultState);
+// 写法三
+const reducer = handleActions({
+    [actions.setLoading().type]: (state, action) => {
+        console.log("reducer里边的action",action);
+        return state.set("loading", action.payload)
+    },
+    [actions.setAccount().type]: (state, {payload}) =>  state.set("accounts", payload),
+    [actions.setAll().type]: (state, {payload}) =>  state.set("all", payload)
+  }, defaultState);
 export default reducer;
